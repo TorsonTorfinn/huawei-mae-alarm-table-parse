78491b0721084e7e98a77ee12b52f048c7aa4e1f
Suhrob Yusubakhmedov <torsontorfinn@gmail.com>
2025-08-07 09:58:16 +0500

    new scraper for nce cleared alarms by end time

--- fourth.py
+++ fourth.py
@@ -59,19 +59,60 @@ try:
 
     # Ожидание загрузки страницы после авторизации
     WebDriverWait(driver, 20).until(EC.url_contains(url_contains_env))
+    
 
 
-    iframe = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.TAG_NAME, "iframe"))
-    )
-    driver.switch_to.frame(iframe)
+    time.sleep(10)
+    # Найти все iframe на странице
+    iframes = driver.find_elements(By.TAG_NAME, "iframe")
+    print(f"Количество iframe: {len(iframes)}")
 
-    # Нахождение и клик на кнопку Export
+    export_button_found = False
+
+    for index, iframe in enumerate(iframes):
+        try:
+            # Переключаемся на iframe
+            WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it(iframe))
+            print(f"Переключились на iframe {index + 1}")
+
+            # Пытаемся найти кнопку Export
+            export_button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#exportBtn > button > span")))
+            
+            print(f"Кнопка 'Export' найдена в iframe {index + 1}")
+            export_button.click()
+            print("Кнопка 'Export' успешно нажата.")
+            export_button_found = True
+            break
+        except Exception as e:
+            print(f"Кнопка 'Export' не найдена в iframe {index + 1}: {e}")
+        finally:
+            # Возвращаемся в главный контекст, чтобы переключиться на следующий iframe
+            driver.switch_to.default_content()
+
+    if not export_button_found:
+        print("Кнопка 'Export' не найдена ни в одном iframe.")
+
+
     # time.sleep(10)
-    export_button = WebDriverWait(driver, 30).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[@id="exportBtn"]/button'))
-    )
-    export_button.click()
+    # if len(driver.find_elements(By.TAG_NAME, "iframe")) > 0:
+    #     print("Количество iframes на странице:", len(driver.find_elements(By.TAG_NAME, "iframe")))
+
+
+
+    # iframe = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "iframe")))
+    # driver.switch_to.frame(iframe)
+
+    # time.sleep(10)
+    # export_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="exportBtn"]/button')))
+    # export_button.click()
+
+    # if export_button:
+    #     export_button.click()
+    #     print("Кнопка 'Export' успешно нажата с использованием XPath.")
+    # else:
+    #     print("Кнопка 'Export' не найдена.")
+
+
 
     # Выбор "All" в выпадающем меню
     all_option = WebDriverWait(driver, 20).until(
